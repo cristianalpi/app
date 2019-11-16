70e3a9d6aa3ec78b13d2c92efb793dffb82ac1f2
Cristian Alvarado <cristianalpi@unisabana.edu.co>
2019-11-16 08:36:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Examen.hola <code>src/App.js</code> and save to reload.
+          Esto es es para un 4.9 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
